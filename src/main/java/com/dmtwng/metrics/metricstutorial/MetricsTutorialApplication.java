ca012ccfe408a0354a313caa44f41d7c9586f71b
Dmytro Patserkovskyi <dmytro.wng@gmail.com>
2022-09-16 22:17:02 +0200

    init

---
+++ src/main/java/com/dmtwng/metrics/metricstutorial/MetricsTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.dmtwng.metrics.metricstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmtwng/metrics/metricstutorial/MetricsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmtwng.metrics.metricstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
