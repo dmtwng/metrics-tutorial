ab3b54e51d1d0c4727df20afc87dff72e02cf93d
Dmytro Patserkovskyi <dmytro.wng@gmail.com>
2022-11-22 23:24:16 +0100

    added counters for register/unregister

--- src/main/java/com/dmtwng/metrics/metricstutorial/controller/CallCenterController.java
+++ src/main/java/com/dmtwng/metrics/metricstutorial/controller/CallCenterController.java
@@ -16,23 +16,39 @@ public class CallCenterController {
 
     private Set<String> operators = new HashSet<>();
 
+    private final Counter registeredCounter;
+    private final Counter unregisteredCounter;
+
+
     public CallCenterController(MeterRegistry meterRegistry) {
         // Register gauge to get size from collection
         Gauge.builder("callcenter.operators.number", operators::size)
                 .strongReference(true)
                 .description("Custom metric, get number of operators in call center")
                 .register(meterRegistry);
+        this.registeredCounter = Counter.builder("callcenter.operators.registered.count")
+                .description("Custom metric, count or successful operator register events")
+                .register(meterRegistry);
+        this.unregisteredCounter = Counter.builder("callcenter.operators.unregistered.count")
+                .description("Custom metric, count or successful operator unregister events")
+                .register(meterRegistry);
     }
 
     @GetMapping("/register")
     public String registerOperator(@RequestParam String id) {
-        operators.add(id);
+        if (!operators.contains(id)) {
+            operators.add(id);
+            registeredCounter.increment();
+        }
         return "Operators count: %d".formatted(operators.size());
     }
 
     @GetMapping("/unregister")
     public String unregisterOperator(@RequestParam String id) {
-        operators.remove(id);
+        if (operators.contains(id)) {
+            operators.remove(id);
+            unregisteredCounter.increment();
+        }
         return "Operators count: %d".formatted(operators.size());
     }
 
